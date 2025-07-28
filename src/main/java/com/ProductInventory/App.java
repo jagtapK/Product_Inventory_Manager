0f9962f4031cb8e860bfeaee7644bed2ae00c603
jagtapK <jagtapkajal54@gmail.com>
2025-07-28 13:32:59 +0530

    Fix Main Class

--- src/main/java/com/ProductInventory/App.java
+++ src/main/java/com/ProductInventory/App.java
@@ -2,52 +2,59 @@ package com.ProductInventory;
 
 import java.util.Scanner;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println("Product Inventory Manager! ");
-        
-        System.out.println("Choose an option:" + "\n1.Add New Product" + "\n2.Search By product name"
-			+ "\n3.Delete Product By ID" + "\n4.Update Quantity"+ "\n5.Details" + "\n6.Exit");
-    
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your Choice");
-        
-        int choice = sc. nextInt();
-        sc.nextLine();
-        
-        switch(choice) {
-        
-        case 1:
-        	Add_Products ae = new Add_Products();
-            ae.AddProduct();
-            break;
-             
-        case 2:
-        	SearchByName an = new SearchByName();
-        	an.SearchProducts();
-        	break;
-        	
-        case 3:
-        	DeleteProductByID de = new DeleteProductByID();
-        	de.DeleteDetails();
-        	break;
-        	
-        case 4:
-        	UpdateQuantity ue = new UpdateQuantity();
-        	ue.UpdateEmpDetails();
-        	break;
-        	
-        case 5:
-        	Details d = new Details();
-        	d.details();
-    	
-        case 6:
-        	System.out.println("Exiting program. Goodbye!");
-    		System.exit(0); // Terminates the program immediately
-    		break;
-        }
-    
-    }
+public class App {
+	public static void main(String[] args) {
+		
+		System.out.println("Product Inventory Manager! ");
+
+		System.out.println("Choose an option:" + "\n1.Add New Product" + "\n2.Search By product name"
+				+ "\n3.Delete Product By ID" + "\n4.Update Quantity" + "\n5.Details" + "\n6.Exit");
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your Choice");
+
+		int choice = sc.nextInt();
+		sc.nextLine();
+
+		switch (choice) {
+
+		case 1:
+			// Add new Product Details
+			Add_Products ae = new Add_Products();
+			ae.AddProduct();
+			break;
+
+		case 2:
+			// Search Product By Name
+			SearchByName an = new SearchByName();
+			an.SearchProducts();
+			break;
+
+		case 3:
+			// Delete Details By Id
+			DeleteProductByID de = new DeleteProductByID();
+			de.DeleteDetails();
+			break;
+
+		case 4:
+			// Update Product Quantity
+			UpdateQuantity ue = new UpdateQuantity();
+			ue.UpdateEmpDetails();
+			break;
+
+		case 5:
+			// Add Details for Mapping
+			Details d = new Details();
+			d.details();
+
+		case 6:
+			System.out.println("Exiting program. Goodbye!");
+			System.exit(0); // Terminates the program immediately
+			break;
+
+		default:
+			System.out.println("Enter Valid Input");
+		}
+
+	}
 }
